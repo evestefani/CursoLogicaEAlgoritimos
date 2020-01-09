6baddc60af7ab5984d7b10c115611c6a38239c4d
Estefani Voigt <estefani.voigt@hbsis.com.br>
2020-01-09 12:20:22 -0300

    Primeiro Projeto e Saida e Processamento de Dados

---
+++ PrimeiroProjeto/PrimeiroProjeto/EstruturaSequencial/SaidaDeDados.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace PrimeiroProjeto.EstruturaSequencial
+{
+    class SaidaDeDados
+    {
+        //    //////Write não faz a quebra de linhas, somente o WriteLine
+        //    Console.WriteLine("Olá Mundo!");
+        //    Console.WriteLine("Bom dia!");
+        //    Console.WriteLine();
+
+        //    ////Exemplo
+        //    double x = 10.35784;
+        //    int y = 31;
+        //    string z = "Estefani";
+        //    char w = 'F';
+
+        //    ////minha variavel
+        //    Console.WriteLine(x);
+        //    ////o numero de casas decimais antes da virgula
+        //    Console.WriteLine(x.ToString("F2"));
+        //    ////o numero de casas decimais depois da virgula
+        //    Console.WriteLine(x.ToString("F4"));
+        //    ////Dependendo do idioma que você está usando, você se obriga a usar a biblioteca 
+        //    ////System.Globalization : CultureInfo.InvariantCulture, para imprimir a pontução
+        //    ////correta.
+        //    Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+
+        //    ////concatenações
+        //    Console.WriteLine();
+        //    Console.WriteLine("Resultado = " + x);
+        //    Console.WriteLine("O valor do troco é " + x + " reais");
+        //    Console.WriteLine("O valor do troco é " + x.ToString("F2") + " reais");
+
+        //    Console.WriteLine();
+        //    Console.WriteLine("A paciente " + z + " tem " + y + " anos e seu sexo é " + w);
+
+        //    Console.ReadLine();
+        //}
+    }
+}
